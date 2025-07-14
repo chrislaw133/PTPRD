2ff619c377cdd2e989e7f960958575d0617ed851
chrislaw133 <chrislaw133@gmail.com>
2025-07-13 23:29:09 -0400

    Update justmunge.R

--- Scripts/TWAS/justmunge.R
+++ Scripts/TWAS/justmunge.R
@@ -2,10 +2,10 @@ library(data.table)
 library(devtools)
 require(GenomicSEM)
 
-munge(c("/deac/bio/lackGrp/lawrcm22/TWAS/sumstats/MDD.txt",
-  "/deac/bio/lackGrp/lawrcm22/TWAS/sumstats/ADHD.txt",
-  "/deac/bio/lackGrp/lawrcm22/TWAS/sumstats/BPD.txt"),
-  hm3 = "$WORKDIR/GenomeSEM/snpslist/w_hm3.snplist",
+munge(c("/path/to/MDD.txt",
+  "/path/to/ADHD.txt",
+  "/path/to/BPD.txt"),
+  hm3 = "/path/to/w_hm3.snplist",
   trait.names = c("MDD", "ADHD", "BPD"),
   info.filter = 0.9,
   maf.filter = 0.01)
