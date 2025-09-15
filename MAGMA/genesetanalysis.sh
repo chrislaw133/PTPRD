3630674c5ab46741ef2ba17ab8a9b79c65d97431
chrislaw133 <chrislaw133@gmail.com>
2025-09-15 13:26:34 -0400

    Update genesetanalysis.sh

--- MAGMA/genesetanalysis.sh
+++ MAGMA/genesetanalysis.sh
@@ -6,7 +6,6 @@ Data_File="$WORKDIR/path/to/genes.raw"
 Annot_File="$WORKDIR/AUX/C5.annot"
 Output_Prefix=""
 
-cd /deac/bio/lackGrp/lawrcm22/MAGMA/
 ./magma \
  --gene-results $Data_File \
  --set-annot $Annot_File \
